dd6dc443cecaf83cdcd5445b1ba35f97b708ec92
b.konwischer <b.konwischer@DESKTOP-P0NTEHA.schenk.local>
2018-06-20 22:29:59 +0200

    ...

--- MessengerMaven/src/main/java/messenger/Implementation/CommunicationImpl.java
+++ MessengerMaven/src/main/java/messenger/Implementation/CommunicationImpl.java
@@ -1,5 +1,8 @@
 package messenger.Implementation;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +11,9 @@ import messenger.Interface.Communication;
 @Service
 @Scope("singleton")
 public class CommunicationImpl implements Communication {
+	
+    @PersistenceContext
+	private EntityManager em;
 
 	public boolean sendMessage(String txt, int user_id, int chat_id) {
 		// TODO Auto-generated method stub
